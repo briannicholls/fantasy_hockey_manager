5c573554be0f19b8bccb1be602668c08e2933e46
Brien Bennett <brienbennett@hotmail.com>
2022-10-06 16:23:41 -0400

    Updated API Service, Constants

--- API_serv.py
+++ API_serv.py
@@ -1,134 +1,97 @@
-#Use NHL API service to collect and update information about Players, Teams, Games, etc.
+# Use NHL API service to collect and update information about Players, Teams, Games, etc.
 from cmd import IDENTCHARS
 import json
+from tkinter import END
 from urllib import response
 import requests
+from Constants import *
+#########################################################################################
+### http get to retrieve NHL API JSON
 
+def http_get(endpoint):
+    api_response = requests.get(BASE_URL+endpoint, params={"Content-Type": "application/json"})
+    return api_response.json()
 
+#########################################################################################
 ## Services for Player class
 ##
 
-def get_player(id):
-    response = requests.get("{}/people/{}".format(BASE_URL, id), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    # print(json_data['people'][0]['fullName'])
-    return json_data['people'][0]   
-#test print   
-# get_player(8471214)
+def get_player(player_id):
+    player_attributes = http_get(ENDPOINT_DICT['get_player'].format(player_id))
+    return player_attributes['people'][0]
 
 
-from Constants import *
-
-def get_game_log(id):
-    response = requests.get("{}/people/{}/stats?stats=gameLog&season={}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    game_log = json_data['stats'][0]['splits']
-    # print (game_log[0])
-    # gamePK "The Game" = The first 4 digits identify the season of the game (ie. 2017 for the 2017-2018 season). The next 2 digits give the type of game, where 01 = preseason, 02 = regular season, 03 = playoffs, 04 = all-star. The final 4 digits identify the specific game number. 
-    return game_log
-#test print   
-# get_game_log(8471214)
+def get_game_log(player_id):
+    game_log = http_get(ENDPOINT_DICT['get_game_log'].format(player_id, CURRENT_SEASON))
+    return game_log['stats'][0]['splits']
 
 
-def single_season_stat(id):
-    response = requests.get("{}/people/{}/stats?stats=statsSingleSeason&season={}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    season_stat = json_data['stats'][0]['splits'][0]
-    # print (season_stat)
-    return season_stat
-#test print   
-# single_season_stat(8471214)
+def season_stat(player_id):
+    season_stats = http_get(ENDPOINT_DICT['season_stat'].format(player_id,CURRENT_SEASON))
+    return season_stats['stats'][0]['splits'][0]
 
 
-def div_stat_split(id):
-    response = requests.get("{}/people/{}/stats?stats=vsDivision&season={}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    div_stats = json_data['stats'][0]['splits']
-    return div_stats
-
-# NEW THINGS
-# params:
-#   endpoint: '/people/:id/stats'
-def http_get(endpoint, params={}):
-    params_string = format_params_to_string(params)
-    api_response = requests.get("{BASE_URL}/{endpoint}?{params_string}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"})
-    return api_response.json()
-
-def div_stat_splitp(player_id):
-    player_stats_response = http_get('/people/{player_id}/stats', { "stats": "byMonth" })
-    return player_stats_response['stats'][0]['splits']
-# END NEW THINGS
-
-def mon_stat_split(id):
-    response = requests.get("{}/people/{}/stats?stats=byMonth&season={}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    mon_stat = json_data['stats'][0]['splits']
-    return mon_stat
-
-def week_stat_split(id):
-    response = requests.get("{}/people/{}/stats?stats=byDayOfWeek&season={}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    week_stat = json_data['stats'][0]['splits']
-    return week_stat
-
-def vs_league_stat(id):
-    response = requests.get("{}/people/{}/stats?stats=regularSeasonStatRankings&season={}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    league_stat = json_data['stats'][0]['splits']
-    return league_stat
-
-def reg_season_pace(id):
-    #only works in season
-    response = requests.get("{}/people/{}/stats?stats=onPaceRegularSeason&season={}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    season_pace = json_data['stats'][0]['splits']
-    return season_pace
-
-def reg_season_pace(id):
-    return requests.get(
-        "{}/people/{}/stats?stats=onPaceRegularSeason&season={}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"}
-    ).json()['stats'][0]['splits']
-
-def home_away_split(id):
-    response = requests.get("{}/people/{}/stats?stats=homeAndAway&season={}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    home_away = json_data['stats'][0]['splits']
-    return home_away
+def div_split(player_id):
+    div_stats = http_get(ENDPOINT_DICT['div_split'].format(player_id,CURRENT_SEASON))
+    return div_stats['stats'][0]['splits'][0]
 
 
-def win_loss_split(id):
-    response = requests.get("{}/people/{}/stats?stats=winLoss&season={}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    win_loss = json_data['stats'][0]['splits']
-    return win_loss
+def mon_split(player_id):
+    mon_stats = http_get(ENDPOINT_DICT['mon_split'].format(player_id,CURRENT_SEASON))
+    return mon_stats['stats'][0]['splits'][0]
+
+
+def week_split(player_id):
+    week_split = http_get(ENDPOINT_DICT['week_split'].format(player_id,CURRENT_SEASON))
+    return week_split['stats'][0]['splits'][0]
+
+
+def league_stat(player_id):
+    league_stat = http_get(ENDPOINT_DICT['league_stat'].format(player_id,CURRENT_SEASON))
+    return league_stat['stats'][0]['splits'][0]
+
+
+def season_pace(player_id):
+    #only works in season per API
+    season_pace = http_get(ENDPOINT_DICT['season_pace'].format(player_id,CURRENT_SEASON))
+    return season_pace['stats'][0]['splits']
+
+
+def sit_goals(player_id):
+    sit_goals = http_get(ENDPOINT_DICT['sit_goals'].format(player_id,CURRENT_SEASON))
+    return sit_goals['stats'][0]['splits'][0]
+
+
+def day_split(player_id):
+    day_split = http_get(ENDPOINT_DICT['day_split'].format(player_id,CURRENT_SEASON))
+    return day_split['stats'][0]['splits']
+
+
+def home_away_split(player_id):
+    home_away_split = http_get(ENDPOINT_DICT['home_away_split'].format(player_id,CURRENT_SEASON))
+    return home_away_split['stats'][0]['splits']
+
+
+def win_loss_split(player_id):
+    win_loss_split = http_get(ENDPOINT_DICT['win_loss_split'].format(player_id,CURRENT_SEASON))
+    return win_loss_split['stats'][0]['splits']
 
 
 def player_schedule(id):
     pass
     ## needs to look up the player's team and then pull the teams schedule for the player
-    # response = requests.get("{}/people/{}/stats?stats=winLoss&season={}".format(BASE_URL, id,CURRENT_SEASON), params={"Content-Type": "application/json"})
-    # json_data = response.json()
-    # win_loss = json_data['stats'][0]['splits']
-    # return win_loss
-
 
 
 ## Services for Game class
 ##
 
-def game_box(id):
-    response = requests.get("{}/game/{}/boxscore".format(BASE_URL, id), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    game_box = json_data['teams']
-    # print (game_box)
-    return game_box
+def game_box(game_id):
+    game_box = http_get(ENDPOINT_DICT['game_box'].format(game_id))
+    return game_box['teams']
 
 
-def game_live(id):
-    response = requests.get("{}/game/{}/feed/live".format(BASE_URL, id), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    game_live = json_data
-    # print (game_live)
+def game_live(game_id):
+    game_live = http_get(ENDPOINT_DICT['game_live'].format(game_id))
     return game_live
 
 
@@ -136,27 +99,21 @@ def game_live(id):
 ##
 
 
-def team_IDs():
-    response = requests.get("{}/teams", params={"Content-Type": "application/json"})
-    json_data = response.json()
-    team_IDs = json_data['teams']
-    # print (team_IDs)
-    return team_IDs
+def team_id():
+    team_id = http_get(ENDPOINT_DICT['team_id'])
+    return team_id['teams']
 
 
-def team_schedule(id):
-    response = requests.get("{}/schedule?teamId={}&startDate={}&endDate={}".format(BASE_URL, id,START_DATE,END_DATE), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    team_schedule = json_data
-    print (team_schedule)
-    return team_schedule
+def team_schedule(team_id):
+    team_schedule = http_get(ENDPOINT_DICT['team_schedule'].format(team_id, START_DATE, END_DATE))
+    return team_schedule['dates']
 
 
-def team_roster(id):
-    response = requests.get("{}/teams/{}?expand=team.roster".format(BASE_URL, id), params={"Content-Type": "application/json"})
-    json_data = response.json()
-    team_roster = json_data['teams'][0]['roster']['roster']
-    print (team_roster)
-    return team_roster
+def team_roster(team_id):
+    team_roster = http_get(ENDPOINT_DICT['team_roster'].format(team_id))
+    return team_roster['dates']
+
+
+  
 
 
--- Constants.py
+++ Constants.py
@@ -3,4 +3,26 @@ CURRENT_SEASON = "20212022"
 LAST_SEASON = "20202021"
 START_DATE = "2022-10-05"
 END_DATE = "2022-04-15"
-BASE_URL="https://statsapi.web.nhl.com/api/v1"
+BASE_URL='https://statsapi.web.nhl.com/api/v1'
+
+
+ENDPOINT_DICT=  {
+    'get_player' :      '/people/{}',
+    'get_game_log' :    '/people/{}/stats?stats=gameLog&season={}',
+    'season_stat' :     '/people/{}/stats?stats=statsSingleSeason&season={}',
+    'div_split' :       '/people/{}/stats?stats=vsDivision&season={}',
+    'mon_split' :       '/people/{}/stats?stats=byMonth&season={}',
+    'week_split' :      '/people/{}/stats?stats=byDayOfWeek&season={}',
+    'league_stat' :     '/people/{}/stats?stats=regularSeasonStatRankings&season={}',
+    'season_pace' :     '/people/{}/stats?stats=onPaceRegularSeason&season={}',
+    'sit_goals' :       '/people/{}/stats?stats=goalsByGameSituation&season={}',
+    'day_split' :       '/people/{}/stats?stats=byDayOfWeek&season={}',
+    'home_away_split' : '/people/{}/stats?stats=homeAndAway&season={}',
+    'win_loss_split' :  'people/{}/stats?stats=winLoss&season={}',
+    'game_box' :        '/game/{}/boxscore',
+    'game_live' :       '/game/{}/feed/live',
+    'team_id' :         '/teams',
+    'team_schedule' :   '/schedule?teamId={}&startDate={}&endDate={}',
+    'team_roster' :     '/teams/{}?expand=team.roster',
+    }
+
