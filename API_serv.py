ff23035084a1b7f8c7c2ba0b5f06a9c7ec2f02e9
Brien Bennett <brienbennett@hotmail.com>
2022-10-14 11:33:08 -0400

    clean up / refactor

--- API_serv.py
+++ API_serv.py
@@ -18,64 +18,64 @@ def http_get(endpoint):
 ##
 
 def get_player(player_id):
-    player_attributes = http_get(ENDPOINT_DICT['get_player'].format(player_id))
-    return player_attributes['people'][0]
+    api_response = http_get(ENDPOINT_DICT['get_player'].format(player_id))
+    return api_response['people']
 
 
 def get_game_log(player_id):
-    game_log = http_get(ENDPOINT_DICT['get_game_log'].format(player_id, CURRENT_SEASON))
-    return game_log['stats'][0]['splits']
+    api_response = http_get(ENDPOINT_DICT['get_game_log'].format(player_id, CURRENT_SEASON))
+    return api_response['stats'][0]['splits']
 
 
 def season_stat(player_id):
-    season_stats = http_get(ENDPOINT_DICT['season_stat'].format(player_id,CURRENT_SEASON))
-    return season_stats['stats'][0]['splits']
+    api_response = http_get(ENDPOINT_DICT['season_stat'].format(player_id,CURRENT_SEASON))
+    return api_response['stats'][0]['splits']
 
 
 def div_split(player_id):
-    div_stats = http_get(ENDPOINT_DICT['div_split'].format(player_id,CURRENT_SEASON))
-    return div_stats['stats'][0]['splits']
+    api_response = http_get(ENDPOINT_DICT['div_split'].format(player_id,CURRENT_SEASON))
+    return api_response['stats'][0]['splits']
 
 
 def mon_split(player_id):
-    mon_stats = http_get(ENDPOINT_DICT['mon_split'].format(player_id,CURRENT_SEASON))
-    return mon_stats['stats'][0]['splits']
+    api_response = http_get(ENDPOINT_DICT['mon_split'].format(player_id,CURRENT_SEASON))
+    return api_response['stats'][0]['splits']
 
 
 def week_split(player_id):
-    week_split = http_get(ENDPOINT_DICT['week_split'].format(player_id,CURRENT_SEASON))
-    return week_split['stats'][0]['splits']
+    api_response = http_get(ENDPOINT_DICT['week_split'].format(player_id,CURRENT_SEASON))
+    return api_response['stats'][0]['splits']
 
 
 def league_stat(player_id):
-    league_stat = http_get(ENDPOINT_DICT['league_stat'].format(player_id,CURRENT_SEASON))
-    return league_stat['stats'][0]['splits']
+    api_response = http_get(ENDPOINT_DICT['league_stat'].format(player_id,CURRENT_SEASON))
+    return api_response['stats'][0]['splits']
 
 
 def season_pace(player_id):
     #only works in season per API
-    season_pace = http_get(ENDPOINT_DICT['season_pace'].format(player_id,CURRENT_SEASON))
-    return season_pace['stats'][0]['splits']
+    api_response = http_get(ENDPOINT_DICT['season_pace'].format(player_id,CURRENT_SEASON))
+    return api_response['stats'][0]['splits']
 
 
 def sit_goals(player_id):
-    sit_goals = http_get(ENDPOINT_DICT['sit_goals'].format(player_id,CURRENT_SEASON))
-    return sit_goals['stats'][0]['splits']
+    api_response = http_get(ENDPOINT_DICT['sit_goals'].format(player_id,CURRENT_SEASON))
+    return api_response['stats'][0]['splits']
 
 
 def day_split(player_id):
-    day_split = http_get(ENDPOINT_DICT['day_split'].format(player_id,CURRENT_SEASON))
-    return day_split['stats'][0]['splits']
+    api_response = http_get(ENDPOINT_DICT['day_split'].format(player_id,CURRENT_SEASON))
+    return api_response['stats'][0]['splits']
 
 
 def home_away_split(player_id):
-    home_away_split = http_get(ENDPOINT_DICT['home_away_split'].format(player_id,CURRENT_SEASON))
-    return home_away_split['stats'][0]['splits']
+    api_response = http_get(ENDPOINT_DICT['home_away_split'].format(player_id,CURRENT_SEASON))
+    return api_response['stats'][0]['splits']
 
 
 def win_loss_split(player_id):
-    win_loss_split = http_get(ENDPOINT_DICT['win_loss_split'].format(player_id,CURRENT_SEASON))
-    return win_loss_split['stats'][0]['splits']
+    api_response = http_get(ENDPOINT_DICT['win_loss_split'].format(player_id,CURRENT_SEASON))
+    return api_response['stats'][0]['splits']
 
 
 def player_schedule(id):
@@ -87,13 +87,13 @@ def player_schedule(id):
 ##
 
 def game_box(game_id):
-    game_box = http_get(ENDPOINT_DICT['game_box'].format(game_id))
-    return game_box['teams']
+    api_response = http_get(ENDPOINT_DICT['game_box'].format(game_id))
+    return api_response['teams']
 
 
 def game_live(game_id):
-    game_live = http_get(ENDPOINT_DICT['game_live'].format(game_id))
-    return game_live
+    api_response = http_get(ENDPOINT_DICT['game_live'].format(game_id))
+    return api_response
 
 
 ## Services for Team class
@@ -102,18 +102,18 @@ def game_live(game_id):
 
 # get all teams
 def fetch_teams():
-    response = http_get(ENDPOINT_DICT['fetch_teams'])
-    return response['teams']
+    api_response = http_get(ENDPOINT_DICT['fetch_teams'])
+    return api_response['teams']
 
 
 def team_schedule(team_id):
-    team_schedule = http_get(ENDPOINT_DICT['team_schedule'].format(team_id, START_DATE, END_DATE))
-    return team_schedule['dates']
+    api_response = http_get(ENDPOINT_DICT['team_schedule'].format(team_id, START_DATE, END_DATE))
+    return api_response['dates']
 
 
 def team_roster(team_id):
-    team_roster = http_get(ENDPOINT_DICT['team_roster'].format(team_id))
-    return team_roster['teams'][0]['roster']['roster']
+    api_response = http_get(ENDPOINT_DICT['team_roster'].format(team_id))
+    return api_response['teams'][0]['roster']['roster']
 
 
 
