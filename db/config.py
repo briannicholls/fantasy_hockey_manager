d12663c9bad33c021911565f22421b6c7347dafd
Brian Nicholls <nichol88@gmail.com>
2022-10-15 15:53:06 -0400

    wip - connection set up and fetching records

--- db/config.py
+++ db/config.py
@@ -1,13 +1,33 @@
 import psycopg2
 
 # Connect to your postgres DB
-conn = psycopg2.connect("dbname=fantasy_hockey_manager user=postgres")
+# returns Connection if successful
+conn = psycopg2.connect("dbname=fantasy_hockey_manager_bn user=postgres host='localhost' password=test1234" )
+
+print("PostgreSQL server information")
+print(conn.get_dsn_parameters(), "\n")
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
 
 # Execute a query
-cur.execute("SELECT * FROM teams")
+cur.execute("""
+CREATE TABLE IF NOT EXISTS players (
+  id serial,
+  external_id VARCHAR   UNIQUE NOT NULL,
+  full_name   VARCHAR   NOT NULL
+);
+CREATE TABLE IF NOT EXISTS teams (
+  id serial,
+  external_id VARCHAR   UNIQUE NOT NULL,
+  name        VARCHAR   NOT NULL
+);
+
+INSERT INTO "teams"   VALUES ( 1, 'XXX-XXX' , 'Test Team' ) ON CONFLICT DO NOTHING;
+INSERT INTO "players" VALUES ( 2, 'XXX-XXX' , 'Test Player' ) ON CONFLICT DO NOTHING;
+
+SELECT * FROM teams;
+""")
 
 # Retrieve query results
 records = cur.fetchall()
